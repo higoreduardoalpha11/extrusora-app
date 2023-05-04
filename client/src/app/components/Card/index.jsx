97e808d5f1f5e418def9a77018cc4ab952859ba5
higoreduardoalpha11 <higoreduardoalpha11@gmail.com>
2023-05-04 19:03:43 -0300

    :lock: Fix emergency energy project

--- client/src/app/components/Card/index.jsx
+++ client/src/app/components/Card/index.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import eventBus from '@/app/utils/eventBus';
 import { Icon, Button } from '@/app/components';
-
 import './index.css';
 
 const Card = (props) => {
+  const [isNoReady, setIsNoReady] = useState(true);
+
+  useEffect(() => {
+    // Listener/Clear on-ready
+    eventBus.on('on-ready', (data) => setIsNoReady(data));
+    eventBus.remove('on-ready');
+  }, []);
+
   return (
     <article className="card flex flex-row flex-start gap-20 p-20 bg-bg-light border-radius-lg">
       <div className="card--icon">
@@ -21,6 +30,7 @@ const Card = (props) => {
             content={props.isOn ? 'On' : 'Off'}
             color={props.isOn ? 'success' : 'none'}
             handleOnClick={props.handleOnClick}
+            isDisable={isNoReady}
           />
         </p>
 
