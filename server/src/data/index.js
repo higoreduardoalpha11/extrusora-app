7fb9ddab90fde3e213f77601f9b44c736a00025a
higoreduardoalpha11 <higoreduardoalpha11@gmail.com>
2023-05-07 17:22:19 -0300

    :seedling: Seed and test log crud

--- server/src/data/index.js
+++ server/src/data/index.js
@@ -50,6 +50,12 @@ const zonesId = [
   new mongoose.Types.ObjectId(),
   new mongoose.Types.ObjectId(),
 ];
+const logsId = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+]
 
 const entryZoneTemplate = {
   _id: '',
@@ -80,4 +86,36 @@ const generateZones = (zone) => {
 
   return zones;
 }
-module.exports = generateZones;
+const generateLogs = () => {
+  return [
+    {
+      _id: logsId[0],
+      type: 'error',
+      title: 'Limite de pressão ultrapassado',
+      description: 'Limite de pressão de trabalho está superior ao limite máximo de segurança, por favor, desligue o equipamento e analise suas condições de trabalho.',
+      visible: false,
+    },
+    {
+      _id: logsId[1],
+      type: 'warning',
+      title: 'Limite de pressão próximo',
+      description: 'Limite de pressão de trabalho está próximo ao limite máximo de segurança, por favor, tome cuidado e analise suas condições de trabalho.',
+      visible: false,
+    },
+    {
+      _id: logsId[2],
+      type: 'success',
+      title: 'Peça acabada',
+      description: 'Peça finalizada durante o ciclo de trabalho, por motivos de segurança o equipamento foi desligado.',
+      visible: false,
+    },
+    {
+      _id: logsId[3],
+      type: 'success',
+      title: 'Processo concluído',
+      description: 'Processamento finalizado com sucesso, por motivos de segurança o equipamento foi desligado.',
+      visible: false,
+    },
+  ]
+}
+module.exports = { generateZones, generateLogs };
