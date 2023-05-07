5343b1629313e7fc24daf0ac1841b1f4f9bf4334
higoreduardoalpha11 <higoreduardoalpha11@gmail.com>
2023-05-07 10:31:57 -0300

    :sparkles: Create chart line header

--- client/src/app/scenes/Dashboard/index.jsx
+++ client/src/app/scenes/Dashboard/index.jsx
@@ -1,9 +1,3 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { MdDeviceThermostat } from 'react-icons/md';
-
-import { Analytics } from '@/app/action';
-import eventBus from '@/app/utils/eventBus';
 import {
   CardExtruder,
   CardRate,
@@ -14,39 +8,24 @@ import {
 } from '@/app/components';
 
 const Dashboard = () => {
-  const { temperatures } = useSelector((state) => state.analytics);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(
-      Analytics.handleChangeTemperatures('2023-05-04', (message) => {
-        if (message) eventBus.disptach('toast', message);
-      })
-    )
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
   return (
     <section className="dashboard py-30">
-      <div className="container">
-        <div className="grid grid-3 gap-20 pb-20">
+      <div className="container flex flex-col gap-20">
+        <div className="grid grid-3 gap-20">
           <CardExtruder />
           <CardRate />
         </div>
 
-        <div className="grid grid-3 gap-20 pb-20">
+        <div className="grid grid-3 gap-20">
           <CardEntry />
           <CardMix />
-          <CardDosage />          
+          <CardDosage />
         </div>
 
-        <article className="flex flex-col gap-20 p-20 bg-bg-light border-radius-lg">
-          <span className="flex flex-row flex-start gap-30 font-sm">
-            <MdDeviceThermostat style={{ fontSize: 50 }} />
-            Níveis de temperatura
-          </span>
-          
-          <ChartLine data={temperatures} />
-        </article>
+        <ChartLine target="temperatures" />
+        <ChartLine target="pressures" />
+        <ChartLine target="powers" />
+        <ChartLine target="rates" />
       </div>
     </section>
   )
